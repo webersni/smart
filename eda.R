d8dc01c966fb4ce82679e3bdb12d828d733ceeb8
webersni <nicolas.webersinke@outlook.com>
2021-01-16 15:12:26 +0100

    Add comments for tasks

--- eda.R
+++ eda.R
@@ -6,14 +6,17 @@ allocations <- read_csv("data/allocations.csv")
 
 # 3.2
 # 1.
-amount_allocated <- allocations %>% count(Code) #%>% select(n) %>% sum()
-amount_with_data <- data %>% count(Code) #%>% select(n) %>% sum()
+# Get number of participants
+number_allocated <- allocations %>% count(Code) #%>% select(n) %>% sum()
+number_with_data <- data %>% count(Code) #%>% select(n) %>% sum()
 
 # 2.
+# Get fraction which have electric heating systems
 fraction_residential <- sum(data$Code == 1 & (data[47] == 1 | data[48] == 1 | data[56] == 1 | data[57] == 1), na.rm = TRUE) / amount_with_data$n[1]
 fraction_sme <- sum(data$Code == 2 & data[173] == 1, na.rm = TRUE) / amount_with_data$n[2]
 fraction_other <- sum(data$Code == 3 & (data[47] == 1 | data[48] == 1 | data[56] == 1 | data[57] == 1), na.rm = TRUE) / amount_with_data$n[3]
 
+# Get fraction with NAs
 fraction_residential_na <- sum(data$Code == 1 & (is.na(data[47]) & is.na(data[48]) & is.na(data[56]) & is.na(data[57]))) / amount_with_data$n[1]
 fraction_sme_na <- sum(data$Code == 2 & is.na(data[173])) / amount_with_data$n[2]
 fraction_other_na <- sum(data$Code == 3 & (is.na(data[47]) & is.na(data[48]) & is.na(data[56]) & is.na(data[57]))) / amount_with_data$n[3]
