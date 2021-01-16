6109dde36eb5aa211dc1a69f575c5438b2d17f6f
webersni <nicolas.webersinke@outlook.com>
2021-01-16 14:41:00 +0100

    Add eda script with tasks 3.2.1 and 3.2.2

---
+++ eda.R
@@ -0,0 +1,19 @@
+library(readr)
+library(dplyr)
+
+data <- read_csv("data/data_RES_SME_profiles_collapsed_merged.csv")
+allocations <- read_csv("data/allocations.csv")
+
+# 3.2
+# 1.
+amount_allocated <- allocations %>% count(Code) #%>% select(n) %>% sum()
+amount_with_data <- data %>% count(Code) #%>% select(n) %>% sum()
+
+# 2.
+fraction_residential <- sum(data$Code == 1 & (data[47] == 1 | data[48] == 1 | data[56] == 1 | data[57] == 1, na.rm = TRUE) / amount_with_data$n[1]
+fraction_sme <- sum(data$Code == 2 & data[173] == 1, na.rm = TRUE) / amount_with_data$n[2]
+fraction_other <- sum(data$Code == 3 & (data[47] == 1 | data[48] == 1 | data[56] == 1 | data[57] == 1), na.rm = TRUE) / amount_with_data$n[3]
+
+fraction_residential_na <- sum(data$Code == 1 & (is.na(data[47]) & is.na(data[48]) & is.na(data[56]) & is.na(data[57]))) / amount_with_data$n[1]
+fraction_sme_na <- sum(data$Code == 2 & is.na(data[173])) / amount_with_data$n[2]
+fraction_other_na <- sum(data$Code == 3 & (is.na(data[47]) & is.na(data[48]) & is.na(data[56]) & is.na(data[57]))) / amount_with_data$n[3]
